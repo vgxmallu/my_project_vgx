be9fb33ca2a8f2495a47b2323e18fa7be36b98fd
vgxmallu <DeltaAlgor@outlook.com>
2026-02-15 22:35:55 +0530

    Create __main__.py

---
+++ vgx/__main__.py
@@ -0,0 +1,28 @@
+import asyncio
+import datetime
+from client import app, scheduler
+from database import db
+from plugins.jobs import send_scheduled_message
+
+async def restore_jobs():
+    print("♻️  Restoring schedules...")
+    async for job in db.get_active_jobs():
+        if job["next_run"] > datetime.datetime.now():
+            scheduler.add_job(
+                send_scheduled_message, 
+                "date", 
+                run_date=job["next_run"], 
+                args=[str(job["_id"])],
+                id=str(job["_id"])
+            )
+
+if __name__ == "__main__":
+    scheduler.start()
+    
+    # Restore jobs on startup
+    loop = asyncio.get_event_loop()
+    loop.create_task(restore_jobs())
+    
+    print("🚀 Bot Started!")
+    app.run()
+  
