e695135ddaa637c37ba677dd20b8c3120f98fd5a
vgxmallu <DeltaAlgor@outlook.com>
2026-02-16 07:44:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 import os
 
-class Config:
-    API_ID = int(os.environ.get("API_ID", "12345"))
-    API_HASH = os.environ.get("API_HASH", "your_hash_here")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token_here")
-    MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://...")
-    DB_NAME = "scheduler_bot"
 
+
+class Config:
+    API_ID = int(os.environ.get("API_ID", "12345")) # Get from my.telegram.org
+    API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN")
+    
+    # MongoDB Connection URL (Get from MongoDB Atlas)
+    MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://...") 
+    
+    DB_NAME = "GroupSchedulerBot"
+    ADMIN_ID = 784589736 # Your Telegram ID (to prevent abuse)
