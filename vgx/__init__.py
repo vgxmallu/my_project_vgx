e78377039bdd7fbdd108fd8559a2cb030acd4719
vgxmallu <DeltaAlgor@outlook.com>
2026-02-16 07:32:05 +0530

    VGX ⚡

--- vgx/__init__.py
+++ vgx/__init__.py
@@ -11,5 +11,5 @@ app = Client(
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
-    plugins=dict(root="plugins") # Automatically loads files in plugins/
+    plugins=dict(root="vgx") # Automatically loads files in plugins/
 )
