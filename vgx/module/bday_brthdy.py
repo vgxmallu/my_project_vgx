5785f6aa1bf72ddf93ecb81cadd05081d19447a4
vgxmallu <DeltaAlgor@outlook.com>
2026-02-19 23:38:26 +0530

    VGX ⚡

--- vgx/module/bday_brthdy.py
+++ vgx/module/bday_brthdy.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from vgx.database.bday_db import *
-from utils import is_admin
+from utils3 import is_admin
 from config import *
 
 @Client.on_message(filters.command("setbirthday") & filters.group)
