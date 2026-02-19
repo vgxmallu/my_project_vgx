766744612f1baca2ba709e2e9b8873b49d3ec76f
vgxmallu <DeltaAlgor@outlook.com>
2026-02-19 22:21:51 +0530

    VGX ⚡

--- vgx/__init__.py
+++ vgx/__init__.py
@@ -2,13 +2,27 @@ from pyrogram import Client
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from config import Config
 import logging
-
+from pyrogram.types import BotCommand
 
 # Setup Logging
 logging.basicConfig(level=logging.INFO)
-
 # Initialize Scheduler
 scheduler = AsyncIOScheduler()
+
+
+
+
+# ... your client setup ...
+
+async def set_ui_commands(app):
+    await app.set_bot_commands([
+        BotCommand("start", "start to know about me;)"),
+        BotCommand("help", "Check my help menu!!"),
+        BotCommand("leaderboard", "View group activity rankings"),
+        BotCommand("global", "See the top users across all groups")
+    ])
+
+
 
 # Initialize Bot
 app = Client(
