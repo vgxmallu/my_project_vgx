d97c1e8f4b517447645effb84c34c61a7ac12589
vgxmallu <DeltaAlgor@outlook.com>
2026-02-19 23:52:21 +0530

    VGX ⚡

--- vgx/__init__.py
+++ vgx/__init__.py
@@ -12,18 +12,6 @@ scheduler = AsyncIOScheduler()
 
 
 
-# ... your client setup ...
-
-async def set_ui_commands(app):
-    await app.set_bot_commands([
-        BotCommand("start", "start to know about me;)"),
-        BotCommand("help", "Check my help menu!!"),
-        BotCommand("leaderboard", "View group activity rankings"),
-        BotCommand("global", "See the top users across all groups")
-    ])
-
-
-
 # Initialize Bot
 app = Client(
     "scheduler_bot_session",
