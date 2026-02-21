80c2d84c1809de0a9bfa5a45ea0fc79b341e1c3d
vgxmallu <DeltaAlgor@outlook.com>
2026-02-21 23:05:52 +0530

    VGX ⚡

--- vgx/database/rss_db.py
+++ vgx/database/rss_db.py
@@ -4,24 +4,24 @@ from config import Config
 class Database:
     def __init__(self):
         self.client = AsyncIOMotorClient(Config.MONGO_URL)
-        self.db = self.client["rss_feed_db"]
+        self.db = self.client["rssfeed_db"]
         self.feeds = self.db.feeds
 
-    async def add_feed(self, chat_id, feed_url):
+    async def add_feed(self, chat_id: int, feed_url: str):
         feed_data = {
             "chat_id": chat_id,
             "feed_url": feed_url,
             "is_active": True,
-            "template": "<b>{{title}}</b>\n{{content}}\n<a href='{{link}}'>Read more...</a>",
+            "template": "<b>{{title}}</b>\n\n{{content}}\n\n<a href='{{link}}'>Read more...</a>",
             "parse_mode": "html",
             "preview_enabled": True,
             "images_enabled": True,
             "notifications_enabled": True,
-            "posted_entries": [] # Cache of posted guids
+            "posted_entries": [] # Cache
         }
         await self.feeds.update_one(
             {"chat_id": chat_id, "feed_url": feed_url},
-            {"$set": feed_data},
+            {"$setOnInsert": feed_data},
             upsert=True
         )
 
@@ -29,26 +29,28 @@ class Database:
         cursor = self.feeds.find({"is_active": True})
         return await cursor.to_list(length=None)
 
-    async def get_chat_feeds(self, chat_id):
+    async def get_chat_feeds(self, chat_id: int):
         cursor = self.feeds.find({"chat_id": chat_id})
         return await cursor.to_list(length=None)
 
-    async def update_feed_setting(self, chat_id, feed_url, setting_key, setting_value):
-        await self.feeds.update_one(
-            {"chat_id": chat_id, "feed_url": feed_url},
-            {"$set": {setting_key: setting_value}}
-        )
+    async def get_feed(self, feed_id: str):
+        from bson.objectid import ObjectId
+        return await self.feeds.find_one({"_id": ObjectId(feed_id)})
 
-    async def add_to_cache(self, chat_id, feed_url, entry_id):
-        await self.feeds.update_one(
-            {"chat_id": chat_id, "feed_url": feed_url},
-            {"$push": {"posted_entries": entry_id}}
-        )
+    async def update_setting(self, feed_id: str, key: str, value):
+        from bson.objectid import ObjectId
+        await self.feeds.update_one({"_id": ObjectId(feed_id)}, {"$set": {key: value}})
 
-    async def clear_cache(self, chat_id, feed_url):
-        await self.feeds.update_one(
-            {"chat_id": chat_id, "feed_url": feed_url},
-            {"$set": {"posted_entries": []}}
-        )
+    async def add_to_cache(self, feed_id: str, entry_id: str):
+        from bson.objectid import ObjectId
+        await self.feeds.update_one({"_id": ObjectId(feed_id)}, {"$push": {"posted_entries": entry_id}})
+
+    async def clear_cache(self, feed_id: str):
+        from bson.objectid import ObjectId
+        await self.feeds.update_one({"_id": ObjectId(feed_id)}, {"$set": {"posted_entries": []}})
+
+    async def delete_feed(self, feed_id: str):
+        from bson.objectid import ObjectId
+        await self.feeds.delete_one({"_id": ObjectId(feed_id)})
 
 db = Database()
