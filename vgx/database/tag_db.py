7f8ea9b0dca2ee72add5966c1cf6cb6a1683ae1c
vgxmallu <DeltaAlgor@outlook.com>
2026-02-22 20:33:53 +0530

    VGX ⚡

--- vgx/database/tag_db.py
+++ vgx/database/tag_db.py
@@ -2,28 +2,15 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from config import Config
 
 client = AsyncIOMotorClient(Config.MONGO_URL)
-db = client["NotifyDB"]
+db = client["NotifyBot"]
 users = db.users
 
-async def get_user_settings(user_id):
+async def get_user_config(user_id):
     user = await users.find_one({"user_id": user_id})
     if not user:
-        # Default settings
+        # Default settings: Complete style, Unmuted
         return {"mode": "complete", "muted": False}
     return user
 
-async def update_setting(user_id, key, value):
+async def update_config(user_id, key, value):
     await users.update_one({"user_id": user_id}, {"$set": {key: value}}, upsert=True)
-
-
-async def add_hashtag(user_id, hashtag):
-    await users.update_one(
-        {"user_id": user_id},
-        {"$addToSet": {"hashtags": hashtag}},
-        upsert=True
-    )
-
-async def get_hashtags(user_id):
-    user = await users.find_one({"user_id": user_id})
-    return user.get("hashtags", []) if user else []
-    
