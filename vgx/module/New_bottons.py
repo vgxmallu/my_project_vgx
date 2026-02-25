bf0962764116cb5435b4e737b6cec2710ac25096
vgxmallu <DeltaAlgor@outlook.com>
2026-02-25 07:47:42 +0530

    VGX ⚡

--- vgx/module/New_bottons.py
+++ vgx/module/New_bottons.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
+from pyrogram.enums import ButtonStyle
 # ... your client setup ...
 
 @Client.on_message(filters.command("colors"))
@@ -8,9 +8,9 @@ async def send_colored_grid(client, message):
     
     # 1. Define the colored buttons
     # You can add the new 'style' parameter to set their colors
-    btn_green = InlineKeyboardButton("Green Button", url="https://t.me/your_bot", style="success")
-    btn_blue = InlineKeyboardButton("Blue Button", url="https://t.me/your_bot", style="primary")
-    btn_red = InlineKeyboardButton("Red Button", url="https://t.me/your_bot", style="danger")
+    btn_green = InlineKeyboardButton("Green Button", url="https://t.me/your_bot", icon_custom_emoji_id=5355142851615283756, style=ButtonStyle.SUCCESS)
+    btn_blue = InlineKeyboardButton("Blue Button", url="https://t.me/your_bot", icon_custom_emoji_id=5440389890787281213,  style=ButtonStyle.PRIMARY)
+    btn_red = InlineKeyboardButton("Red Button", url="https://t.me/your_bot", icon_custom_emoji_id=5354968347094046619, style=ButtonStyle.DANGER)
 
     # 2. Create the grid layout just like your image
     keyboard = InlineKeyboardMarkup([
@@ -22,3 +22,5 @@ async def send_colored_grid(client, message):
     text = "🌈 **Here are your colored buttons!**"
     
     await message.reply(text, reply_markup=keyboard)
+
+
