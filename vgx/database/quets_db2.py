74e6676a7144b78e5af862c31c1073f891c63eeb
vgxmallu <DeltaAlgor@outlook.com>
2026-03-01 22:34:21 +0530

    VGX ⚡

--- vgx/database/quets_db2.py
+++ vgx/database/quets_db2.py
@@ -1,6 +1,6 @@
 
 from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_URL
+from config import Config
 
 client = AsyncIOMotorClient(Config.MONGO_URL)
 db = client["random_quotes_bot"]
