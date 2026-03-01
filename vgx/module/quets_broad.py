7282e0ccfce9b05ef779b65a128590c8cb154f0b
vgxmallu <DeltaAlgor@outlook.com>
2026-03-01 11:39:12 +0530

    VGX ⚡

--- vgx/module/quets_broad.py
+++ vgx/module/quets_broad.py
@@ -4,6 +4,7 @@ from pyrogram.errors import FloodWait, PeerIdInvalid, UserIsBlocked
 from vgx.database.quets_db import get_all_enabled_chats, disable_quotes
 from quotes_list import POWERFUL_QUOTES
 from vgx.module.quets_broad import send_hourly_quotes
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 async def send_hourly_quotes(app):
     """This function is called by the APScheduler every hour."""
