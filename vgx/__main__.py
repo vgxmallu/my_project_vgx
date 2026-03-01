50b49ff603a2a18c16efe17a4f65828be1bc8ef8
vgxmallu <DeltaAlgor@outlook.com>
2026-03-01 11:29:32 +0530

    VGX ⚡

--- vgx/__main__.py
+++ vgx/__main__.py
@@ -11,15 +11,14 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from vgx.module.night_schedul import start_nm_scheduler
 from vgx.module.dfeed_scheduler import start_df_scheduler
 from vgx.module.anylz_schedul import start_anlyz_scheduler
-from vgx.module.bday_schedul import check_celebrations
-from vgx.module.quets_broad import send_hourly_quotes
-
+from vgx.module.bday_schedul import start_bday_scheduler
+from vgx.module.quets_broad import start_qet_scheduler
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("SchedulerBot")
 
 
-scheduler = AsyncIOScheduler()
+
 
 
 async def restore_jobs():
@@ -49,7 +48,7 @@ async def restore_jobs():
 if __name__ == "__main__":
     scheduler.start()
     app.start()
-
+    
     print("💫 Night Mode System Online.")
     start_nm_scheduler(app)
 
@@ -57,14 +56,14 @@ if __name__ == "__main__":
     start_df_scheduler(app)
     
     print("🤖 Golden Hour Analytics Online...")
-    start_anlyz_scheduler()
+    start_anlyz_scheduler(app)
 
-    scheduler.add_job(check_celebrations, "interval", hours=1, args=[app])
-    scheduler.add_job(send_hourly_quotes, "interval", hours=1, args=[app])
-    scheduler.start()
-    print("🎂 Birthday & Event Scheduler is Live....")
     print("🚀 Motivation Bot is Online ⏰ Scheduler started")
- 
+    start_qet_scheduler(app)
+    
+    print("🎂 Birthday & Event Scheduler is Live....")
+    start_bday_scheduler(app)
+    
     loop = asyncio.get_event_loop()
     loop.create_task(restore_jobs())
     
