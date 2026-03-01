8d5d63a3ea3d9204e00a16681758d4e1f904dab6
vgxmallu <DeltaAlgor@outlook.com>
2026-03-01 21:22:44 +0530

    VGX ⚡

--- vgx/database/quets_db2.py
+++ vgx/database/quets_db2.py
@@ -1,1 +1,26 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import Config
+import time
+
+client = AsyncIOMotorClient(Config.MONGO_URL)
+db = client["GoldenHour_DB"]
+chats = db.chats
+
+async def get_chat_data(chat_id):
+    chat = await chats.find_one({"chat_id": chat_id})
+    return chat or {
+        "chat_id": chat_id,
+        "enabled": False,
+        "interval": 60,      # Default 1h (in minutes)
+        "delete_after": 0,   # 0 means disabled
+        "pin": False,
+        "last_msg_id": None,
+        "last_sent": 0
+    }
+
+async def update_chat(chat_id, **kwargs):
+    await chats.update_one({"chat_id": chat_id}, {"$set": kwargs}, upsert=True)
+
+async def get_all_active_chats():
+    return chats.find({"enabled": True})
 
