96247ac0c17b144af3bd1501321c2a0e956bb25b
vgxmallu <DeltaAlgor@outlook.com>
2026-03-05 15:09:51 +0530

    VGX ⚡

--- vgx/module/night_m_keybord.py
+++ vgx/module/night_m_keybord.py
@@ -1,4 +1,5 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.enums import ButtonStyle
 
 def get_settings_kb(data):
     # Visual Toggles
@@ -13,26 +14,23 @@ def get_settings_kb(data):
     stk = "🟢" if p['stickers'] else "🔴"
     
     return InlineKeyboardMarkup([
-        [InlineKeyboardButton(f"Night Mode: {en}", callback_data="nm_toggle_main")],
+        [InlineKeyboardButton(f"Night Mode: {en}", callback_data="nm_toggle_main", style=ButtonStyle.SUCCESS)],
         [
-            InlineKeyboardButton(f"🕒 Start: {data['night_start']}", callback_data="nm_set_start"),
-            InlineKeyboardButton(f"☀️ End: {data['night_end']}", callback_data="nm_set_end")
+            InlineKeyboardButton(f"🕒 Start: {data['night_start']}", callback_data="nm_set_start", style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton(f"☀️ End: {data['night_end']}", callback_data="nm_set_end", style=ButtonStyle.PRIMARY)
         ],
         [InlineKeyboardButton(f"🌍 Timezone: {data['timezone']}", callback_data="nm_set_tz")],
-        [InlineKeyboardButton("--- 🛡 RESTRICTIONS ---", callback_data="ignore")],
+        [InlineKeyboardButton("--- 🛡 RESTRICTIONS ---", callback_data="ignore", style=ButtonStyle.SUCCESS)],
         [
-            InlineKeyboardButton(f"Text {txt}", callback_data="nm_perm_text"),
-            InlineKeyboardButton(f"Media {med}", callback_data="nm_perm_media"),
-            InlineKeyboardButton(f"Stickers {stk}", callback_data="nm_perm_stickers")
+            InlineKeyboardButton(f"📝Text {txt}", callback_data="nm_perm_text", style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton(f"📹 Media {med}", callback_data="nm_perm_media", style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton(f"🎭 Stickers {stk}", callback_data="nm_perm_stickers", style=ButtonStyle.PRIMARY)
         ],
-        [InlineKeyboardButton("--- ⚙️ EXTRAS ---", callback_data="ignore")],
+        [InlineKeyboardButton("--- ⚙️ EXTRAS ---", callback_data="ignore", style=ButtonStyle.SUCCESS)],
         [
             InlineKeyboardButton(f"⚠️ Warning: {warn}", callback_data="nm_toggle_warn"),
-            InlineKeyboardButton(f"🧹 Auto-Clean: {clean}", callback_data="nm_toggle_clean")
-        ],
-        [
+            InlineKeyboardButton(f"🧹 Auto-Clean: {clean}", callback_data="nm_toggle_clean"),
             InlineKeyboardButton("🚨 Emergency Unlock", callback_data="nm_emergency"),
-            InlineKeyboardButton("👑 Manage VIPs", callback_data="nm_vips")
         ],
-        [InlineKeyboardButton("❌ Close Menu", callback_data="nm_close")]
+        [InlineKeyboardButton("❌ Close Menu", callback_data="nm_close", style=ButtonStyle.DANGER)]
     ])
