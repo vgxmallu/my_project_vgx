2ffbb5a9282468f4ba3640929eff398075561e0e
vgxmallu <DeltaAlgor@outlook.com>
2026-06-14 21:35:05 +0530

    VGX ⚡

---
+++ vgx/module/My_Test_shts.py
@@ -0,0 +1,11 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@Client.on_message(filters.private & filters.command("tt"))
+async def start(c, m):
+    await m.reply(
+        "<h1>Hey i am Advanced Scheduler Bot**\n</h1>"
+        "Commands:\n"
+        "/cmd - for my commands\n"
+    )
