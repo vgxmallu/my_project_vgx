6385b5751932fd91fda8f271e8c084325649db13
vgxmallu <DeltaAlgor@outlook.com>
2026-06-28 20:24:01 +0530

    Update My_Test_shts.py

--- vgx/module/My_Test_shts.py
+++ vgx/module/My_Test_shts.py
@@ -4,16 +4,12 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputRich
 
 @Client.on_message(filters.private & filters.command("tt"))
 async def stahgrt(c, m):
-    await m.reply(
-        "<h1>Hey i am Advanced Scheduler Bot\n</h1>"
-        "Commands:\n"
-        "/cmd - for my commands\n"
-    )
-    await c.send_rich_message(
-        chat_id, InputRichMessage("<h1>Hey i am Advanced Scheduler Bot\n</h1>"),
+    await m.send_rich_message(
+        chat.id, InputRichMessage("<h1>Hey i am Advanced Scheduler Bot\n</h1>"),
         reply_markup=InlineKeyboardMarkup(
             [
                 [InlineKeyboardButton("Data", callback_data="callbackigih_data")],
                 [InlineKeyboardButton("Docs", url="https://docs.pyrogram.org")]
             ]))
+
 
