062ad0618b60abe5538a47c0c0ead76570610c01
vgxmallu <DeltaAlgor@outlook.com>
2026-08-26 12:22:44 +0530

    VGX ⚡

--- vgx/database/db_advanc.py
+++ vgx/database/db_advanc.py
@@ -3,7 +3,7 @@ from config import Config
 from bson.objectid import ObjectId
 
 class Database:
-    handlers = None 
+    self.handlers = None
     def __init__(self):
         self.client = AsyncIOMotorClient(Config.MONGO_URL)
         self.db = self.client[Config.DB_NAME]
@@ -25,6 +25,12 @@ class Database:
     async def get_all_jobs(self):
         return self.jobs.find({})
 
+    async def __getattr__(self, name):
+        # Raising AttributeError forces Pyrogram's scanner to completely ignore this object
+        if name == "handlers":
+            raise AttributeError("'SafeDatabase' object has no attribute 'handlers'")
+        return getattr(self._db, name)
+        
     async def update_job(self, job_id, data):
         await self.jobs.update_one({"_id": ObjectId(job_id)}, {"$set": data})
 
@@ -37,3 +43,21 @@ class Database:
 
 db = Database()
 
+from motor.motor_asyncio import AsyncIOMotorClient
+
+# 1. Your existing connection
+_client = AsyncIOMotorClient("YOUR_MONGODB_URI")
+_raw_db = _client["YOUR_DATABASE_NAME"]
+
+# 2. Corrected wrapper
+class SafeDatabase:
+    def __init__(self, db_instance):
+        self._db = db_instance
+
+    
+
+    def __getitem__(self, name):
+        return self._db[name]
+
+# 3. Export
+db = SafeDatabase(_raw_db)
